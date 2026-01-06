351089e34186cf60c5274e00702976041f8632ac
bikramkarki7 <karkibikram756@gmail.com>
2026-01-06 16:08:18 +0545

    a

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Project");
+function findAverage(arr) {
+  let total = sumArray(arr); 
+  return total / arr.length;
+}
+
+let numbers = [3, 1, 4, 1, 5, 9, 2, 6];
+console.log(findAverage(numbers));
