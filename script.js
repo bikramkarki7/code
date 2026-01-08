66d321a537c790a463716d64ada6c7feb7acdafb
bikramkarki7 <karkibikram756@gmail.com>
2026-01-08 18:34:13 +0545

    aaa

--- script.js
+++ script.js
@@ -6,9 +6,21 @@
 
 // let numbers = [3, 1, 4, 1, 5, 9, 2, 6];
 // console.log(findAverage(numbers));
-let a = 1;
-let b = a++;
-let c = ++a;
+// let a = 1;
+// let b = a++;
+// let c = ++a;
 
-console.log(a, b, c);
+// console.log(a, b, c);
+console.log("A");
+
+setTimeout(() => {
+  console.log("B");
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log("C");
+});
+
+console.log("D");
+
 
